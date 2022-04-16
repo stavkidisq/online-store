2f3410e007ecc44d609698fd8b7663d386849d14
stavkidisq <stavkidisq@gmail.com>
2022-04-16 21:28:31 +0300

    feature: add create view to categories controller and edit index view

--- OnlineStore/Areas/Admin/Controllers/CategoriesController.cs
+++ OnlineStore/Areas/Admin/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OnlineStore.Infrastructure;
+using OnlineStore.Models;
 
 namespace OnlineStore.Areas.Admin.Controllers
 {
@@ -13,9 +14,48 @@ namespace OnlineStore.Areas.Admin.Controllers
         {
             _context = context;
         }
+        //GET: admin/categories
         public async Task<IActionResult> Index()
         {
             return View(await _context.Categories.OrderBy(category => category.Sorting).ToListAsync());
+        }
+
+        //GET: admin/categories/create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: /admin/categories/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CategoryModel categoryModel)
+        {
+            if (ModelState.IsValid)
+            {
+                categoryModel.Slug = categoryModel.Name.ToLower().Replace(" ", "-");
+                categoryModel.Sorting = 100;
+
+                var p = await _context.Categories.FirstOrDefaultAsync(x => x.Slug == categoryModel.Slug);
+
+                if (p != null)
+                {
+                    ModelState.AddModelError("", "The category already exists");
+
+                    return View(categoryModel);
+                }
+                else
+                {
+                    _context.Categories.Add(categoryModel);
+                    await _context.SaveChangesAsync();
+
+                    TempData["Success"] = "The category has been added!";
+
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View(categoryModel);
         }
     }
 }
--- OnlineStore/Models/CategoryModel.cs
+++ OnlineStore/Models/CategoryModel.cs
@@ -6,10 +6,9 @@ namespace OnlineStore.Models
     {
         public int Id { get; set; }
         [Required, MinLength(2, ErrorMessage = "Minimum length is 2")]
-        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Only letters are allowed!")]
+        [RegularExpression(@"^[a-zA-Z-]+$", ErrorMessage = "Only letters are allowed!")]
         public string Name { get; set; } = null!;
-        [Required]
-        public string Slug { get; set; } = null!;
+        public string? Slug { get; set; }
         public int Sorting { get; set; }
     }
 }
