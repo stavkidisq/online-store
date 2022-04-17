5339bb5152b394627707a3d0e342da99a52413e3
stavkidisq <stavkidisq@gmail.com>
2022-04-17 13:31:58 +0300

    feature: add image preview to create view in products controller

--- OnlineStore/wwwroot/js/site.js
+++ OnlineStore/wwwroot/js/site.js
@@ -13,3 +13,15 @@
         }, 2000);
     }
 });
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        let reader = new FileReader();
+
+        reader.onload = function (e) {
+            $("img#img-upload").attr("src", e.target.result).width(200).height(200);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
