3023759be3f49d6a7ad347c359d64b14afb14005
stavkidisq <stavkidisq@gmail.com>
2022-04-24 16:03:39 +0300

    feature: add main menu for _layout.cshtml

---
+++ OnlineStore/Infrastructure/MainMenuViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineStore.Models;
+
+namespace OnlineStore.Infrastructure
+{
+    public class MainMenuViewComponent : ViewComponent
+    {
+        private readonly OnlineStoreDbContext _context;
+
+        public MainMenuViewComponent(OnlineStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var pages = await GetPagesAsync();
+
+            return View(pages);
+        }
+
+        private async Task<List<PageModel>> GetPagesAsync()
+        {
+            return await _context.Pages.OrderBy(page => page.Sorting).ToListAsync();
+        }
+    }
+}
