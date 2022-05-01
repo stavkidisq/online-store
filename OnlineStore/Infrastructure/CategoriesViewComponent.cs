fffafa37ce99ce5adba5f826476ec56f2b041bcc
stavkidisq <stavkidisq@gmail.com>
2022-05-01 22:39:15 +0300

    feature: add categories list in _layout

---
+++ OnlineStore/Infrastructure/CategoriesViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineStore.Models;
+
+namespace OnlineStore.Infrastructure
+{
+    public class CategoriesViewComponent : ViewComponent
+    {
+        private readonly OnlineStoreDbContext _context;
+
+        public CategoriesViewComponent(OnlineStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = await GetCategoriesAsync();
+
+            return View(categories);
+        }
+
+        private async Task<List<CategoryModel>> GetCategoriesAsync()
+        {
+            return await _context.Categories.OrderBy(page => page.Sorting).ToListAsync();
+        }
+    }
+}
