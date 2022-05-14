b3a49626cd68b024058a2b561d1085fc2780397a
stavkidisq <stavkidisq@gmail.com>
2022-05-14 16:39:39 +0300

    feature: add CartItemModel.cs

---
+++ OnlineStore/Models/CartItemModel.cs
@@ -0,0 +1,21 @@
+﻿namespace OnlineStore.Models
+{
+    public class CartItemModel
+    {
+        public CartItemModel(ProductModel productModel)
+        {
+            ProductId = productModel.Id;
+            ProductName = productModel.Name;
+            Quantity = 1;
+            Price = productModel.Price;
+            Image = productModel.Image;
+        }
+
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal TotalPrice => Quantity * Price;
+        public string? Image { get; set; }
+    }
+}
