4ffbb3054d11493cfe2f2d0b4b6478cfffed6d5c
stavkidisq <stavkidisq@gmail.com>
2022-07-08 13:22:06 +0900

    feature: add userModel class for authorization in web application

---
+++ OnlineStore/Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿namespace OnlineStore.Models
+{
+    public class UserModel
+    {
+        public string UserName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+
+        UserModel() { }
+
+        public UserModel(AppUserModel appUserModel) 
+        {
+            UserName = appUserModel.UserName;
+            Email = appUserModel.Email;
+            Password = appUserModel.PasswordHash;
+        }
+    }
+}
