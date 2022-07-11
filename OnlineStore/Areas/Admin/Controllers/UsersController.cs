b5d9011c71c687e88e4795d022fdb620490fb5b4
stavkidisq <stavkidisq@gmail.com>
2022-07-11 13:48:14 +0900

    feature: add users list in admin panel

---
+++ OnlineStore/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Models;
+
+namespace OnlineStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<AppUserModel> _userManager;
+
+        public UsersController(UserManager<AppUserModel> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_userManager.Users);
+        }
+    }
+}
