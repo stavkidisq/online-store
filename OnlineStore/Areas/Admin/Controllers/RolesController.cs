89ef717ce170993fd47b60feef54d6630ef775cb
stavkidisq <stavkidisq@gmail.com>
2022-07-12 20:32:34 +0900

    feature: add roles list

---
+++ OnlineStore/Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Models;
+
+namespace OnlineStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<AppUserModel> _userManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<AppUserModel> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_roleManager.Roles);
+        }
+    }
+}
